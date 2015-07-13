ee883e78053c62c64323089058c26e5cbcdd380a
Fredrick Njenga <cybermatatu@gmail.com>
2015-07-13 14:41:40 +0300

    Added the login/register scaffold

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,14 @@ Route::get('/', function () {
 Route::get('cloud/digitalocean', [
     'as' => 'digitalocean', 'uses' => 'test@digitalocean'
 ]);
+
+Route::get('home', 'HomeController@index');
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
